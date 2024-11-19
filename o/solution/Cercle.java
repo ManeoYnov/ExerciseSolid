d6b10fd2b25e730b4f567d30e5a8752e44183234
ManeoYnov <maneo.dangreaux@ynov.com>
2024-11-19 11:50:14 +0100

    Add files via upload

---
+++ o/solution/Cercle.java
@@ -0,0 +1,16 @@
+class Circle extends Shape {
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
